0a1c362992c804c06d67d9d15f6d32974126c87e
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2024-10-16 10:39:31 +0500

    utils added

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@ import express from "express";
 import dotenv from 'dotenv' ;
 
 dotenv.config({path:'./env'}) ;
-
-
-
 import connectDB from "./db/index.js";
-connectDB() ;
+
+import express from "express";
+
+const app = express() ;
+
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`server is runnig at port : ${process.env.PORT}`); 
+  })
+  app.on((error)=>{
+    console.log(error);
+    throw error ;
+  })
+})
+.catch((error)=>{
+  console.log("mongodb connection failed !!!",error);
+}) ;
 
 // const app = express() ;
 // ;(async()=>{
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+// const asyncHandler=(fn)=>async(req,res,next)=>{
+//        try {
+//            await fn(req,res,next)
+//        } catch (err) {
+//             res.status(err.code||500).json({
+//               success:false,
+//               message:err.message
+//             })
+//        }
+// }
+// export default asyncHandler ;
+
+
+const asyncHandler =(reqHandler)=>{
+     (req,res,next)=>{
+      Promise.resolve(reqHandler(req,res,next)).reject((error)=>{
+        next(error)
+      })
+     }
+}
+
+export default asyncHandler ;
