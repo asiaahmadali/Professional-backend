129ae5b65728318dbf6c758f9fa5aa6161ba0cdf
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2024-11-20 16:32:52 +0500

    db connect

--- src/practice.js
+++ src/practice.js
@@ -9,7 +9,7 @@ const app = express() ;
 //           await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`) ;
 //           app.listen(process.env.PORT , (error)=>{
 //             console.log(`server is listening at ${process.env.PORT}`);
-            
+          
 //           });
 //           app.on('error',(error)=>{
 //             console.log(error);
